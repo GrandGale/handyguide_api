d8601c6d303a5e919b868acaf8b56b3e84e23275
Bello Ango <angobello0@gmail.com>
2023-07-27 18:27:49 +0100

    documentation error fix

--- app/department/schemas.py
+++ app/department/schemas.py
@@ -3,11 +3,11 @@ from pydantic import BaseModel, Field
 
 class DepartmentBase(BaseModel):
     name: str = Field(
-        description="Name of the faculty",
+        description="Name of the department",
         max_length=100,
     )
     abbrev: str = Field(
-        description="Abbreviation of the faculty",
+        description="Abbreviation of the department",
         max_length=10,
     )
 
