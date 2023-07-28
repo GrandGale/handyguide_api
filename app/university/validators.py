a0a3aeffa42ed4a983d56e5a276226976309377e
Bello Ango <angobello0@gmail.com>
2023-07-28 21:54:51 +0100

    documented university module

--- app/university/validators.py
+++ app/university/validators.py
@@ -5,9 +5,21 @@ from app.dependencies import get_db
 from app.university import models
 
 
-def university_is_valid(university_abbrev: str, db: Session = Depends(get_db)):
+def university_is_valid(university_abbrev: str, db: Session):
+    """This function checks if the university exists in the db
+
+    Args:
+        university_abbrev (str): The university abbrev
+        db (Session, optional): The DB Session.
+
+    Raises:
+        HTTPException[404]: If the university doesnt exist
+
+    Returns:
+        bool[True]: If the university exists
+    """
     if db.query(models.University).get(university_abbrev):
-        return university_abbrev
+        return True
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail="University Does not exist"
     )
