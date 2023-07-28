2348fdb233ceaa17b3fdcc07c201accf75849b19
Bello Ango <angobello0@gmail.com>
2023-07-28 22:24:16 +0100

    implemented get_level endpoint

--- app/level/api.py
+++ app/level/api.py
@@ -18,3 +18,8 @@ def create_level(level: schemas.LevelCreate, db: Session = Depends(get_db)):
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.Level])
 def get_level_list(db: Session = Depends(get_db)):
     return selectors.get_level_list(db=db)
+
+
+@router.get("/{level}", status_code=status.HTTP_200_OK, response_model=schemas.Level)
+def get_level(level: str, db: Session = Depends(get_db)):
+    return selectors.get_level(level_abbrev=level, db=db)
--- app/level/selectors.py
+++ app/level/selectors.py
@@ -1,3 +1,4 @@
+from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
 
 from app.level import models
@@ -13,3 +14,24 @@ def get_level_list(db: Session):
         List[models.Level]: A list of all the levels in the db
     """
     return db.query(models.Level).all()
+
+
+def get_level(level_abbrev: str, db: Session):
+    """This function returns a level obj from the db
+
+    Args:
+        level_abbrev (str): The level abbrev
+        db (Session): The db session
+
+    Raises:
+        HTTPException[404]: If the level doesnt exist
+
+    Returns:
+        models.Level: The level obj
+    """
+    level = db.query(models.Level).filter_by(abbrev=level_abbrev).first()
+    if not level:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Level Not Found"
+        )
+    return level
