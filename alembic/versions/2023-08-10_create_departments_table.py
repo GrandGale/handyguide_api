16b690e030aab5b08e10a7645dbe650ea40fcfae
Bello Ango <angobello0@gmail.com>
2023-08-10 23:04:41 +0100

    created departments migration and fixed bug

--- alembic/versions/2023-08-10_create_departments_table.py
+++ alembic/versions/2023-08-10_create_departments_table.py
@@ -1,8 +1,8 @@
-"""created faculties table
+"""create departments table
 
-Revision ID: 31a0e3f46050
-Revises: 83b94a4fea5a
-Create Date: 2023-08-10 22:13:59.137209
+Revision ID: 8916184d747c
+Revises: 31a0e3f46050
+Create Date: 2023-08-10 22:42:20.523268
 
 """
 from alembic import op
@@ -10,25 +10,27 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "31a0e3f46050"
-down_revision = "83b94a4fea5a"
+revision = "8916184d747c"
+down_revision = "31a0e3f46050"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
     op.create_table(
-        "faculties",
+        "departments",
         sa.Column("id", sa.Integer, primary_key=True),
         sa.Column("name", sa.String(100)),
         sa.Column("abbrev", sa.String(10), index=True),
+        sa.Column("faculty", sa.String()),
         sa.Column("university", sa.String()),
+        sa.ForeignKeyConstraint(["faculty"], ["faculties.abbrev"], ondelete="CASCADE"),
         sa.ForeignKeyConstraint(
             ["university"], ["universities.abbrev"], ondelete="CASCADE"
         ),
-        sa.UniqueConstraint("name", "university", name="unique_faculty"),
+        sa.UniqueConstraint("name", "faculty", "university", name="unique_department"),
     )
 
 
 def downgrade() -> None:
-    op.drop_table("faculties")
+    op.drop_table("departments")
