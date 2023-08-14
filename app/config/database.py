4974bd5b12b296caa874b52228b7697681cdd5ab
Bello Ango <angobello0@gmail.com>
2023-08-14 18:56:14 +0100

    added postgres veriables

--- app/config/database.py
+++ app/config/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from app.config.settings import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    url=settings.POSTGRES_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/config/settings.py
+++ app/config/settings.py
@@ -9,17 +9,30 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media/")
 
 
 class Settings(BaseSettings):
-    DEBUG: bool
-    SESSION: str
-    SQLALCHEMY_DATABASE_URL: str
-    AZURE_STORAGE_ACCOUNT_NAME: str
-    AZURE_STORAGE_ACCOUNT_KEY: str
-    AZURE_CONNECTION_STRING: str
-    AZURE_CONTAINER_NAME: str
-    AZURE_BLOB_URL: str
+    DEBUG: bool = os.environ.get("DEBUG")
+    SESSION: str = os.environ.get("SESSION")
+
+    # DB Settings
+    POSTGRES_USER: str = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.environ.get("POSTGRES_SERVER")
+    POSTGRES_PORT: int = os.environ.get("POSTGRES_PORT")
+    POSTGRES_DB: str = os.environ.get("POSTGRES_DB")
+
+    # Azure Settings
+    AZURE_STORAGE_ACCOUNT_NAME: str = os.environ.get("AZURE_STORAGE_ACCOUNT_NAME")
+    AZURE_STORAGE_ACCOUNT_KEY: str = os.environ.get("AZURE_STORAGE_ACCOUNT_KEY")
+    AZURE_CONNECTION_STRING: str = os.environ.get("AZURE_CONNECTION_STRING")
+    AZURE_CONTAINER_NAME: str = os.environ.get("AZURE_CONTAINER_NAME")
+    AZURE_BLOB_URL: str = os.environ.get("AZURE_BLOB_URL")
 
     class Config:
         env_file = ".env"
+
+    @property
+    def POSTGRES_URL(self):
+        url = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return url
 
 
 settings = Settings()
