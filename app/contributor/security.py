bc76f02dd103214d0d4b6dd9f420cd5617fa0050
Bello Ango <angobello0@gmail.com>
2023-08-15 21:46:55 +0100

    implemented password hasher

---
+++ app/contributor/security.py
@@ -0,0 +1,15 @@
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+
+def hash_password(raw: str) -> str:
+    """This function hashes a password
+
+    Args:
+        raw (str): The raw password
+
+    Returns:
+        str: The hashed password
+    """
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.hash(raw)
