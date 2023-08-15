b3d10493fc0d7a5e8d466d1e0d0e7d28779e445f
Bello Ango <angobello0@gmail.com>
2023-08-15 20:12:24 +0100

    created contributor schema

--- app/contributor/schemas.py
+++ app/contributor/schemas.py
@@ -2,4 +2,26 @@ from pydantic import BaseModel, Field
 
 
 class BaseContributor(BaseModel):
-    pass
+    username: str = Field(
+        description="The Contributor's username", min_length=3, max_length=50
+    )
+    email: str = Field(description="The Contributor's email address", max_length=255)
+    first_name: str = Field(description="The Contributor's first name", max_length=50)
+    last_name: str = Field(description="The Contributor's last name", max_length=50)
+    university_id: str = Field(
+        description="The Contributor's university ID", max_length=50
+    )
+    university: str = Field(description="The Contributor's university", max_length=50)
+    level: str = Field(description="The Contributor's level", max_length=50)
+    faculty: int = Field(description="The Contributor's faculty")
+    department: int = Field(description="The Contributor's department")
+
+
+class ContributorCreate(BaseContributor):
+    password: str
+
+
+class Contributor(BaseContributor):
+    is_contributor: bool
+    is_supervisor: bool
+    is_admin: bool
