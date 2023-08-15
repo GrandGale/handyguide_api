a0908e646eb820a672a86f8d6c4754dc2c47b4e3
Bello Ango <angobello0@gmail.com>
2023-08-15 21:47:08 +0100

    removed BaseUser model

--- app/contributor/models.py
+++ app/contributor/models.py
@@ -12,29 +12,26 @@ from sqlalchemy import (
 from app.config.database import DBBase
 
 
-class BaseUser(DBBase):
+class Contributor(DBBase):
+    __tablename__ = "contributors"
+
     id = Column(Integer, primary_key=True)
     first_name = Column(String(20), nullable=False)
     last_name = Column(String(20), nullable=False)
     username = Column(String(20), nullable=False, unique=True)
     email = Column(String(120), nullable=False, unique=True)
     password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    last_login = Column(DateTime)
-    created_at = Column(DateTime, default=datetime.now())
-
-
-class Contributor(BaseUser):
-    __tablename__ = "contributors"
-
-    university_id = Column(String(30), nullable=False, unique=True)
+    university_id = Column(String(30), nullable=False)
     university = Column(String(10), nullable=False)
     level = Column(String(10), nullable=False)
     faculty = Column(Integer, nullable=False)
     department = Column(Integer, nullable=False)
+    is_active = Column(Boolean, default=True)
     is_contributor = Column(Boolean, default=True)
     is_supervisor = Column(Boolean, default=False)
     is_admin = Column(Boolean, default=False)
+    last_login = Column(DateTime)
+    created_at = Column(DateTime, default=datetime.now())
 
     ForeignKeyConstraint(
         ["university", "faculty", "department"],
