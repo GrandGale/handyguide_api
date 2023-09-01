b7a46dc45b68a36e2f51a6fdb214d9aee768383f
Bello Ango <angobello0@gmail.com>
2023-09-01 21:55:07 +0100

    implemented login and auth functions

--- app/contributor/services.py
+++ app/contributor/services.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 
+from app.config.settings import settings
 from app.contributor import models, schemas
-from app.contributor.security import hash_password
+from app.contributor.security import create_access_token, hash_password, verify_password
 from app.contributor.validators import validate_contributor
 
 
@@ -23,3 +26,51 @@ def create_contributor(contributor: schemas.ContributorCreate, db: Session):
     db.commit()
     db.refresh(obj)
     return obj
+
+
+def login_contributor(contributor: schemas.ContributorLogin, db: Session):
+    """This function logs in a contributor
+
+    Args:
+        contributor (schemas.ContributorLogin): The contirbutor login schema obj
+        db (Session): The DB session
+
+    Returns:
+        schemas.Token: The JWT token
+    """
+    user = authenticate_contributor(
+        email=contributor.email, password=contributor.password, db=db
+    )
+    if user:
+        access_token = create_access_token(
+            data={
+                "sub": user.email,
+                "exp": datetime.now()
+                + timedelta(hours=settings.ACCESS_TOKEN_EXPIRE_HOURS),
+            }
+        )
+        return schemas.Token(access_token=access_token, token_type="Bearer")
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password"
+    )
+
+
+def authenticate_contributor(email: str, password: str, db: Session):
+    """This function authenticates a contributor
+
+    Args:
+        email (str): The contributor's email
+        password (str): The contributor's password
+        db (Session): The DB session
+
+    Returns:
+        bool[False]: If authentication failed
+        models.User: The authenticated user obj
+    """
+    contributor = db.query(models.Contributor).filter_by(email=email).first()
+    if contributor and verify_password(raw=password, hashed=contributor.password):
+        contributor.last_login = datetime.now()
+        db.commit()
+        db.refresh(contributor)
+        return contributor
+    return False
