90078c6a50bd0350d6893315ffc877b00ec61aa6
Bello Ango <angobello0@gmail.com>
2023-09-03 08:30:59 +0100

    removed expired token exception

--- app/rest_exceptions.py
+++ app/rest_exceptions.py
@@ -6,9 +6,3 @@ InvalidTokenException = HTTPException(
     detail="Could not validate token",
     headers={"WWW-Authenticate": "Bearer"},
 )
-
-ExpiredTokenException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Token has expired",
-    headers={"WWW-Authenticate": "Bearer"},
-)
