6f63d40f98a8d27d2cf6b1f3acdfaff207633ebe
Bello Ango <angobello0@gmail.com>
2023-09-03 08:31:20 +0100

    removed expiry check

--- app/contributor/dependencies.py
+++ app/contributor/dependencies.py
@@ -1,5 +1,4 @@
 from jose import jwt, JWTError
-from datetime import datetime
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
@@ -7,7 +6,7 @@ from sqlalchemy.orm import Session
 from app.contributor import selectors
 from app.dependencies import get_db
 from app.config.settings import settings
-from app.rest_exceptions import InvalidTokenException, ExpiredTokenException
+from app.rest_exceptions import InvalidTokenException
 
 
 OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="login")
@@ -22,9 +21,6 @@ def get_current_contributor(
             token, settings.SECRET_KEY, algorithms=[settings.HASHING_ALGORITHM]
         )
         email: str = payload.get("sub")
-        expires: datetime = datetime.fromtimestamp(payload.get("exp"))
-        if expires < datetime.now():
-            raise ExpiredTokenException
         if email is None:
             raise InvalidTokenException
         contributor = selectors.get_contributor(email=email, db=db)
